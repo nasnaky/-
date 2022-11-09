ad92074e0919ae8f5e19779841d4785be85e47e1
정재훈 <81303380+nasnaky@users.noreply.github.com>
2022-11-10 07:50:32 +0900

    입출력과 사칙연산
    단계별 문제 남은 거 풀이

---
+++ 3003.py
@@ -0,0 +1,9 @@
+a, b, c, d, e, f = input().split()
+a = int(a)
+b = int(b)
+c = int(c)
+d = int(d)
+e = int(e)
+f = int(f)
+num = [1, 1, 2, 2, 2, 8]
+print(num[0] - a, num[1] - b, num[2] - c, num[3] - d, num[4] - e, num[5] - f)
