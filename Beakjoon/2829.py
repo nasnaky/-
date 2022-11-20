4ae18bdac20501a8938d9274febec5e69964fa65
정재훈 <81303380+nasnaky@users.noreply.github.com>
2022-11-21 07:51:39 +0900

    오늘의 코테
    11/21

---
+++ Beakjoon/2829.py
@@ -0,0 +1,12 @@
+import sys
+
+num = int(sys.stdin.readline())
+count = 0
+while num >= 0:
+    if num % 5 == 0:
+        count += int(num / 5)
+        print(count)
+        exit(0)
+    num -= 3
+    count += 1
+print(-1)
